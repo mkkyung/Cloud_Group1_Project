81354d0ca555da854641437b959018d4bc983985
mkkyung <cng9323@gmail.com>
2019-10-26 18:23:38 +0900

    10/26

---
+++ WebContent/controller/products/List.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	'sap/ui/model/Sorter',
+	'sap/m/MessageBox'
+], function(JSONModel, Controller, Filter, FilterOperator, Sorter, MessageBox) {
+	"use strict";
+
+	return Controller.extend("Cloud_Group1_ProjectCloud_Group1_Project.controller.products.List", {
+		onInit: function () {
+			this.oRouter = this.getOwnerComponent().getRouter();
+		},
+		onListItemPress: function (oEvent) {
+			var oNextUIState = this.getOwnerComponent().getHelper().getNextUIState(1),
+				productPath = oEvent.getSource().getBindingContext("products").getPath(),
+				product = productPath.split("/").slice(-1).pop();
+
+			this.oRouter.navTo("Detail", {layout: oNextUIState.layout, product: product});
+		},
+	});
+});
