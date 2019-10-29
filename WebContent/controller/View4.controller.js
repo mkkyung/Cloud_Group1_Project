56f983ecc5f155f3c0a8e30f952a1bacdd7ccab6
nohkiseo <grape3267@gmail.com>
2019-10-29 11:10:38 +0900

    kiseo

--- WebContent/controller/View4.controller.js
+++ WebContent/controller/View4.controller.js
@@ -1,11 +1,140 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/core/routing/History",
-	"sap/ui/core/UIComponent"
-], function(Controller, History, UIComponent) {
+	"sap/ui/core/UIComponent",
+	'sap/ui/model/odata/v2/ODataModel',
+	'sap/ui/model/json/JSONModel',
+	'sap/m/Label',
+	'sap/ui/model/Filter'
+], function(Controller, History, UIComponent, ODataModel, JSONModel, Label, Filter) {
 	"use strict";
 
 	return Controller.extend("Cloud_Group1_ProjectCloud_Group1_Project.controller.View4", {
+		onInit: function() {
+//			this.oModel = new JSONModel();
+//			this.oModel.loadData(sap.ui.require.toUrl("sap/ui/comp/sample/filterbar/DynamicPageListReport/model.json"), null, false);
+//			this.getView().setModel(this.oModel);
+		
+	        var sServiceUrl = "proxy/http/zenedus4ap1.zenconsulting.co.kr:50000/";	//CORSerror나면 http:// 를 proxy/http/로
+			sServiceUrl +=  "/sap/opu/odata/sap/ZFIORI_STU07_DEV02_SRV/"; // 여기를 /n/iwfnd/maint_service 에 들어가서 내가 만든 경로를 복사 해와야 함.
+	        var url;
+	
+	        url = "/getData2Set";
+	     
+	        var oDataModel = new sap.ui.model.odata.ODataModel(sServiceUrl,true);
+	        this.oModel = new JSONModel();
+			var data;
+			oDataModel.read(url, null, null, false, function(oData){
+//				data = oData;
+				data = oData.results;
+			});
+			var oModel = new sap.ui.model.json.JSONModel({ "data" : data });
+//			var oModel = new sap.ui.model.json.JSONModel(data); // {results : [] }
+			this.getView().setModel(oModel, "view"); 
+			
+
+			this.aKeys = [
+				"Zname", "Zcategory"
+			];
+			this.oSelectName = this.getSelect("slName");
+			this.oSelectCategory = this.getSelect("slCategory");
+//			this.oSelectSupplierName = this.getSelect("slSupplierName");	//key값을 가지고 value값 찾아온다.
+//			this.oModel.setProperty("/Filter/text", "Filtered by None");
+//			this.addSnappedLabel();
+
+			var oFB = this.getView().byId("filterbar");
+			if (oFB) {
+				oFB.variantsInitialized();
+			}
+		},
+		onToggleHeader: function() {
+			this.getPage().setHeaderExpanded(!this.getPage().getHeaderExpanded());
+		},
+		onToggleFooter: function() {
+			this.getPage().setShowFooter(!this.getPage().getShowFooter());
+		},
+		onSelectChange: function() {
+			var aCurrentFilterValues = [];
+
+			aCurrentFilterValues.push(this.getSelectedItemText(this.oSelectName));
+			aCurrentFilterValues.push(this.getSelectedItemText(this.oSelectCategory));
+			aCurrentFilterValues.push(this.getSelectedItemText(this.oSelectSupplierName));
+
+			this.filterTable(aCurrentFilterValues);
+		},
+
+		filterTable: function(aCurrentFilterValues) {
+			this.getTableItems().filter(this.getFilters(aCurrentFilterValues));
+			this.updateFilterCriterias(this.getFilterCriteria(aCurrentFilterValues));
+		},
+
+		updateFilterCriterias: function(aFilterCriterias) {
+			this.removeSnappedLabel(); /* because in case of label with an empty text, */
+			this.addSnappedLabel(); /* a space for the snapped content will be allocated and can lead to title misalignment */
+			this.oModel.setProperty("/Filter/text", this.getFormattedSummaryText(aFilterCriterias));
+		},
+
+		addSnappedLabel: function() {
+			var oSnappedLabel = this.getSnappedLabel();
+			oSnappedLabel.attachBrowserEvent("click", this.onToggleHeader, this);
+			this.getPageTitle().addSnappedContent(oSnappedLabel);
+		},
+
+		removeSnappedLabel: function() {
+			this.getPageTitle().destroySnappedContent();
+		},
+
+		getFilters: function(aCurrentFilterValues) {
+			this.aFilters = [];
+
+			this.aFilters = this.aKeys.map(function(sCriteria, i) {
+				return new Filter(sCriteria, sap.ui.model.FilterOperator.Contains, aCurrentFilterValues[i]);
+			});
+
+			return this.aFilters;
+		},
+		getFilterCriteria: function(aCurrentFilterValues) {
+			return this.aKeys.filter(function(el, i) {
+				if (aCurrentFilterValues[i] !== "") {
+					return el;
+				}
+			});
+		},
+		getFormattedSummaryText: function(aFilterCriterias) {
+			if (aFilterCriterias.length > 0) {
+				return "Filtered By (" + aFilterCriterias.length + "): " + aFilterCriterias.join(", ");
+			} else {
+				return "Filtered by None";
+			}
+		},
+
+		getTable: function() {
+			return this.getView().byId("idProductsTable");
+		},
+		getTableItems: function() {
+			return this.getTable().getBinding("items");
+		},
+		getSelect: function(sId) {
+			return this.getView().byId(sId);
+		},
+		getSelectedItemText: function(oSelect) {
+			return oSelect.getSelectedItem() ? oSelect.getSelectedItem().getKey() : "";
+		},
+		getPage: function() {
+			return this.getView().byId("dynamicPageId");
+		},
+		getPageTitle: function() {
+			return this.getPage().getTitle();
+		},
+		getSnappedLabel: function() {
+			return new Label({
+				text: "{/Filter/text}"
+			});
+		},
+		
+		
+		
+//		__________________________________________________________________
 		onShow : function(){
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("view5");
@@ -22,6 +151,14 @@ sap.ui.define([
 				oRouter.navTo("view3", {}, true);
 			}
 		},
+		
+		onPress: function (oEvent) {
+			var oItem = oEvent.getSource();
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("view5", {
+				ContractPath: oItem.mAggregations.cells[1].mProperties.text
+			});
+		}
 		
 	});
 });
