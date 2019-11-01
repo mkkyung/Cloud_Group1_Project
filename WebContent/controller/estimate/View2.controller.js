2a142d5dbc1462d19cb56fb2ae968ee12600c66e
jin6126 <jin6126js@gmail.com>
2019-11-01 15:42:51 +0900

    1101 view4

--- WebContent/controller/estimate/View2.controller.js
+++ WebContent/controller/estimate/View2.controller.js
@@ -5,22 +5,22 @@ sap.ui.define([
 	'sap/ui/model/odata/v2/ODataModel',
 	'sap/ui/model/json/JSONModel',
 	'sap/m/Label',
-	'sap/ui/model/Filter'
-], function(Controller, History, UIComponent, ODataModel, JSONModel, Label, Filter) {
+	'sap/ui/model/Filter',
+	'sap/m/MessageBox'
+], function(Controller, History, UIComponent, ODataModel, JSONModel, Label, Filter, MessageBox) {
 	"use strict";
 
 	return Controller.extend("Cloud_Group1_ProjectCloud_Group1_Project.controller.estimate.View2", {
+		
+		onInit: function() {
+			this.getData();
+		},
 		
 //		_______________________________________
 		
 		onShow : function(){
 			var oRouter = UIComponent.getRouterFor(this);
 			oRouter.navTo("view3");
-		},
-		
-		onShow2 : function(){
-			var oRouter = UIComponent.getRouterFor(this);
-			oRouter.navTo("estlookup");
 		},
 		
 		goBack : function(oEvent) {
@@ -39,19 +39,30 @@ sap.ui.define([
 	        var sServiceUrl = "proxy/http/zenedus4ap1.zenconsulting.co.kr:50000"; // 로컬 서버 연결 하는 거 
 	        sServiceUrl += "/sap/opu/odata/sap/Z_FUNC_ESTIMATE_TEST_SRV";   // 여기를 /n/iwfnd/maint_service 에 들어가서 내가 만든 경로를 복사 해와야 함.
 	        var url;
-	
-	     
 	        url = "/getestSet";
 	     
-	
 	        var oDataModel = new sap.ui.model.odata.ODataModel(sServiceUrl, true);
 	        var data;
 	        oDataModel.read(url, null, null, false, function (oData) {
 	           data = oData.results;
 	        });
 	        var oModel = new sap.ui.model.json.JSONModel({ "data": data });
-	        this.getView().setModel(oModel);
-		}
+	        this.getView().setModel(oModel , "estlist");
+		},
+		
+		onPress : function (oEvent) {	//계약서 눌렀을 때 
+			var oItem = oEvent.getSource();
+			var oRouter = UIComponent.getRouterFor(this);
+			var routerData = oItem.mAggregations.cells[0].mProperties.text;
+//			routerData = oItem.getBindingContext("estlist").getPath().substr(1);
+			this.onClose(oRouter, 0);
 
+		},
+		
+		onClose: function(oRouter, routerData){
+			oRouter.navTo("estlookup", {
+				EstPath: routerData
+			});
+		}
 	});
 });
