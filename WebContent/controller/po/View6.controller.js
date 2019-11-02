3331ea8a6b315751960400a0ca88f44a660413da
kee0315 <56660351+kee0315@users.noreply.github.com>
2019-11-02 16:26:32 +0900

    view6,view7 create 11/02

--- WebContent/controller/po/View6.controller.js
+++ WebContent/controller/po/View6.controller.js
@@ -6,6 +6,42 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("Cloud_Group1_ProjectCloud_Group1_Project.controller.po.View6", {
+		onInit: function() {
+			this.getData();
+		},
+		
+//		_______________________________________
+			
+		getData : function(){
+	        var sServiceUrl = "proxy/http/zenedus4ap1.zenconsulting.co.kr:50000"; // 로컬 서버 연결 하는 거 
+	        sServiceUrl += "/sap/opu/odata/sap/Z_FUNC_ESTIMATE_TEST_SRV";   // 여기를 /n/iwfnd/maint_service 에 들어가서 내가 만든 경로를 복사 해와야 함.
+	        var url;
+	        url = "/getestSet";
+	     
+	        var oDataModel = new sap.ui.model.odata.ODataModel(sServiceUrl, true);
+	        var data;
+	        oDataModel.read(url, null, null, false, function (oData) {
+	           data = oData.results;
+	        });
+	        var oModel = new sap.ui.model.json.JSONModel({ "data": data });
+	        this.getView().setModel(oModel , "estlist");
+		},
+		
+//		onPress : function (oEvent) {	//계약서 눌렀을 때 
+//			var oItem = oEvent.getSource();
+//			var oRouter = UIComponent.getRouterFor(this);
+//			var routerData = oItem.mAggregations.cells[0].mProperties.text;
+////			routerData = oItem.getBindingContext("estlist").getPath().substr(1);
+//			this.onClose(oRouter, 0);
+//
+//		},
+		
+//		onClose: function(oRouter, routerData){
+//			oRouter.navTo("estlookup", {
+//				EstPath: routerData
+//			});
+//		},
+
 		onShow : function(){
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("view7");
