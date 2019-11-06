c8b55472682d3910d677fac7dd642c9cbe6cf502
jin6126 <jin6126js@gmail.com>
2019-11-06 21:29:34 +0900

    1106 view2,estlookup update

--- WebContent/controller/estimate/View2.controller.js
+++ WebContent/controller/estimate/View2.controller.js
@@ -14,7 +14,13 @@ sap.ui.define([
 		
 		onInit: function() {
 			this.getData();
-//			this.GtCat3Set();
+			this.GtCat3Set();
+			this.GtVenSet();
+//			통화설정
+			var oViewModel = new JSONModel({
+				currency: "KRW"
+			});
+			this.getView().setModel(oViewModel, "view");
 		},
 		
 //		_______________________________________
@@ -50,18 +56,30 @@ sap.ui.define([
 	        var oModel = new sap.ui.model.json.JSONModel({ "data": data });
 	        this.getView().setModel(oModel , "estlist");
 		},
-//		
-//		GtCat3Set : function() {
-//	    	var sServiceUrl= "proxy/http/zenedus4ap1.zenconsulting.co.kr:50000/sap/opu/odata/sap/Z_FUNC_ESTIMATE_TEST_SRV";
-//				
-//	        var url = "/getcat3Set";
-//	        var oDataModel= new sap.ui.model.odata.ODataModel(sServiceUrl,true);
-//	        var data;
-//	        oDataModel.read(url, null, null, false, function(oData) {
-//	        data = oData.results;});
-//	        var oModel = new sap.ui.model.json.JSONModel({ "cat3" : data});
-//	        this.getView().setModel(oModel);
-//	     },
+		
+		GtCat3Set : function() {
+	    	var sServiceUrl= "proxy/http/zenedus4ap1.zenconsulting.co.kr:50000/sap/opu/odata/sap/Z_FUNC_ESTIMATE_TEST_SRV";
+				
+	        var url = "/getcat3Set";
+	        var oDataModel= new sap.ui.model.odata.ODataModel(sServiceUrl,true);
+	        var data;
+	        oDataModel.read(url, null, null, false, function(oData) {
+	        data = oData.results;});
+	        var oModel = new sap.ui.model.json.JSONModel({ "cat3" : data});
+	        this.getView().setModel(oModel, "cat3");
+	     },
+	     
+	     GtVenSet : function() {
+		    	var sServiceUrl= "proxy/http/zenedus4ap1.zenconsulting.co.kr:50000/sap/opu/odata/sap/Z_FUNC_ESTIMATE_TEST_SRV";
+					
+		        var url = "/getvenSet";
+		        var oDataModel= new sap.ui.model.odata.ODataModel(sServiceUrl,true);
+		        var data;
+		        oDataModel.read(url, null, null, false, function(oData) {
+		        data = oData.results;});
+		        var oModel = new sap.ui.model.json.JSONModel({ "ven" : data});
+		        this.getView().setModel(oModel, "ven");
+		     },
 	     
 		onPress : function (oEvent) {	//계약서 눌렀을 때 
 			var oItem = oEvent.getSource();
